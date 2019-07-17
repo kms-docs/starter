9c42b0425320f1c408a9235e64549b74d0666886
evillt <ijoec123@gmail.com>
2019-07-17 22:22:34 +0800

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+new Docute({
+  target: 'app',
+  sourcePath: '/docs',
+  plugins: [
+    docuteCustomContainer()
+  ],
+  nav: [
+    {
+      title: 'GitHub',
+      link: 'https://github.com/kms-docs/starter'
+    }
+  ],
+  sidebar: [
+    {
+      title: '账号',
+      links: [
+        {
+          title: '注册',
+          link: '/account/register'
+        },
+        {
+          title: '保护及解决办法',
+          link: '/account/protect'
+        }
+      ]
+    },
+    {
+      title: '攻略',
+      links: []
+    },
+    {
+      title: '分享',
+      links: [
+        {
+          title: '"智障"的游戏经验以及感想',
+          link: '/share/zhizhang'
+        }
+      ]
+    }
+  ]
+})
