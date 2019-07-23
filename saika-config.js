de146223f3d8134ae6e76c68ffa61ea24f7780ed
evillt <ijoec123@gmail.com>
2019-07-23 22:40:23 +0800

    use Saika

---
+++ saika-config.js
@@ -0,0 +1,36 @@
+new Saika({
+  target: 'app',
+  sourcePath: '/docs',
+  nav: [
+    {
+      title: 'GitHub',
+      link: 'https://github.com/kms-docs/starter'
+    }
+  ],
+  posts: [
+    {
+      title: '账号注册',
+      link: '/account/register'
+    },
+    {
+      title: '保护及解决办法',
+      link: '/account/protect'
+    },
+    {
+      title: '如何解锁',
+      link: '/account/how-to-unlock'
+    },
+    {
+      title: '"智障"的游戏经验以及感想',
+      link: '/share/zhizhang'
+    },
+    {
+      title: '职业对照表',
+      link: '/misc/jobs'
+    },
+    {
+      title: 'NAVER账号邮箱截图方法',
+      link: '/misc/naver-screenshot'
+    }
+  ]
+})
