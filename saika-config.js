676b88ed33d9293c74700565ea50ee30698e7c88
evillt <ijoec123@gmail.com>
2019-08-05 01:21:18 +0800

    use `saika` docs theme

--- saika-config.js
+++ saika-config.js
@@ -1,6 +1,7 @@
 new Saika({
   target: 'app',
   sourcePath: '/docs',
+  theme: 'docs',
   nav: [
     {
       title: 'GitHub',
@@ -9,28 +10,47 @@ new Saika({
   ],
   posts: [
     {
-      title: '账号注册',
-      link: '/account/register'
+      title: '账号',
+      links: [
+        {
+          title: '注册',
+          link: '/account/register'
+        },
+        {
+          title: '保护及解决办法',
+          link: '/account/protect'
+        },
+        {
+          title: '如何解锁',
+          link: '/account/how-to-unlock'
+        }
+      ]
     },
     {
-      title: '保护及解决办法',
-      link: '/account/protect'
+      title: '攻略',
+      links: []
     },
     {
-      title: '如何解锁',
-      link: '/account/how-to-unlock'
+      title: '分享',
+      links: [
+        {
+          title: '"智障"的游戏经验以及感想',
+          link: '/share/zhizhang'
+        }
+      ]
     },
     {
-      title: '"智障"的游戏经验以及感想',
-      link: '/share/zhizhang'
-    },
-    {
-      title: '职业对照表',
-      link: '/misc/jobs'
-    },
-    {
-      title: 'NAVER账号邮箱截图方法',
-      link: '/misc/naver-screenshot'
+      title: '杂项',
+      links: [
+        {
+          title: '职业对照表',
+          link: '/misc/jobs'
+        },
+        {
+          title: 'NAVER账号邮箱截图方法',
+          link: '/misc/naver-screenshot'
+        }
+      ]
     }
   ]
 })
